f544b7b2cfb8a5eb2c6905957c7af39f5834f7ea
lajmia69 <lajmiahmed175@gmail.com>
2025-11-17 22:44:11 +0100

    tp6-act2

---
+++ RMIBanque_Server/src/rmiServer/BanqueServer.java
@@ -0,0 +1,28 @@
+package rmiServer;
+import rmiService.BanqueImpl;
+import rmiService.IBanque;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import java.util.Properties;
+
+public class BanqueServer {
+    public static void main(String[] args) {
+        try {
+            // Configuration JNDI
+            Properties p = new Properties();
+            p.setProperty(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.rmi.registry.RegistryContextFactory");
+            p.setProperty(Context.PROVIDER_URL, "rmi://localhost:1099");
+            
+            // Création du service
+            IBanque banque = new BanqueImpl();
+
+            // Publication via JNDI
+            Context ctx = new InitialContext(p);
+            ctx.bind("Banque", banque);
+
+            System.out.println("Serveur RMI prêt avec JNDI !");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
